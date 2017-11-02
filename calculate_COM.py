159e4c76a167523cc4a5751a28ae73f28a2d912f
skyhoffert <skyhoffert@gmail.com>
2017-11-01 23:10:52 -0400

    Added initial calculate_COM object file

---
+++ calculate_COM.py
@@ -0,0 +1,11 @@
+#
+# File: 		calculate_COM.py
+# Author:		Sky Hoffert
+# Last Modified:	Nov 1, 2017
+# Description:		This file creates an object that is capable of calculating the center of mass for a given configuration of objects
+#			WIP: Inputs can be provided continuously until an entire object is build
+# Python Version:	2.7.3
+#
+
+
+# WIP
